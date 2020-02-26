b5a05c56dd07583a26655c07a4d8178dc7c68709
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2020-02-26 22:05:10 +0300

    encrypted shared preferences

--- app/src/main/java/by/krossovochkin/fiberyunofficial/AuthStorageImpl.kt
+++ app/src/main/java/by/krossovochkin/fiberyunofficial/AuthStorageImpl.kt
@@ -1,11 +1,12 @@
 package by.krossovochkin.fiberyunofficial
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
 import by.krossovochkin.fiberyunofficial.core.data.auth.AuthStorage
 
 private const val PREFS_NAME = "AUTH"
+private const val MASTER_KEY_ALIAS = "fiberyunofficialauth"
 private const val KEY_ACCOUNT = "KEY_ACCOUNT"
 private const val KEY_TOKEN = "KEY_TOKEN"
 
@@ -13,7 +14,13 @@ class AuthStorageImpl(
     context: Context
 ) : AuthStorage {
 
-    private val prefs = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+    private val prefs = EncryptedSharedPreferences.create(
+        PREFS_NAME,
+        MASTER_KEY_ALIAS,
+        context.applicationContext,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 
     private var account: String = prefs.getString(KEY_ACCOUNT, "")!!
     private var token: String = prefs.getString(KEY_TOKEN, "")!!
