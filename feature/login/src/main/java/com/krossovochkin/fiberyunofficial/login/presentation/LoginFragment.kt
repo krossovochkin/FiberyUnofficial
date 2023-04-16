11c865a4d84f3f808213280304e03f97790efed7
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2023-04-16 09:45:45 +0300

    #475 Fix login

--- feature/login/src/main/java/com/krossovochkin/fiberyunofficial/login/presentation/LoginFragment.kt
+++ feature/login/src/main/java/com/krossovochkin/fiberyunofficial/login/presentation/LoginFragment.kt
@@ -19,8 +19,6 @@ package com.krossovochkin.fiberyunofficial.login.presentation
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.webkit.JavascriptInterface
-import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.krossovochkin.core.presentation.flow.collect
@@ -28,14 +26,6 @@ import com.krossovochkin.core.presentation.result.parentListener
 import com.krossovochkin.core.presentation.viewbinding.viewBinding
 import com.krossovochkin.fiberyunofficial.login.R
 import com.krossovochkin.fiberyunofficial.login.databinding.LoginFragmentBinding
-
-private const val MOCK_USER_AGENT = "FiberyUnofficial"
-private const val JS_INTERFACE_NAME = "FiberyUnofficial"
-private const val FIBERY_IO_WEBSITE = "https://fibery.io"
-private const val JS_EXTRACT_TOKEN =
-    "fetch(`https://\${window.location.host}/api/tokens`, { method: 'POST' })\n" +
-        "  .then(res => res.json())\n" +
-        "  .then(obj => ${JS_INTERFACE_NAME}.onTokenReceived(obj.value))"
 
 class LoginFragment(
     factoryProducer: () -> LoginViewModelFactory
@@ -59,41 +49,11 @@ class LoginFragment(
             }
         }
 
-        binding.loginWebView.apply {
-            webViewClient = WebViewClient()
-            settings.apply {
-                userAgentString = MOCK_USER_AGENT
-                javaScriptEnabled = true
-            }
-            val callback = { token: String -> login(token) }
-            addJavascriptInterface(TokenExtractor(callback), JS_INTERFACE_NAME)
-            loadUrl(FIBERY_IO_WEBSITE)
-        }
-        binding.extractButton.setOnClickListener {
-            binding.loginWebView.evaluateJavascript(JS_EXTRACT_TOKEN, null)
-        }
-    }
-
-    private fun login(token: String) {
-        binding.loginWebView.post {
-            viewModel.login(extractAccount(binding.loginWebView.url), token)
-        }
-    }
-
-    private fun extractAccount(url: String?): String {
-        return url
-            .orEmpty()
-            .substringAfter("https://")
-            .substringBefore(".fibery.io")
-    }
-
-    class TokenExtractor(
-        private val callback: (String) -> Unit
-    ) {
-
-        @JavascriptInterface
-        fun onTokenReceived(token: String) {
-            callback(token)
+        binding.loginButton.setOnClickListener {
+            viewModel.login(
+                binding.accountTextInput.editText?.text?.toString() ?: "",
+                binding.tokenTextInput.editText?.text?.toString() ?: "",
+            )
         }
     }
 
