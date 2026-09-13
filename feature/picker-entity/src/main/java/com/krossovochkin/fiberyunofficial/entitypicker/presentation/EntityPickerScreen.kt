916b0ee7d439d0c36cb5a38774e9da6324e5cd9e
Vasya Drobushkov <vasya@drobushkov.com>
2026-09-13 19:31:49 +0200

    Add clickable import to EntityPickerScreen.kt

--- feature/picker-entity/src/main/java/com/krossovochkin/fiberyunofficial/entitypicker/presentation/EntityPickerScreen.kt
+++ feature/picker-entity/src/main/java/com/krossovochkin/fiberyunofficial/entitypicker/presentation/EntityPickerScreen.kt
@@ -17,6 +17,7 @@
 package com.krossovochkin.fiberyunofficial.entitypicker.presentation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
